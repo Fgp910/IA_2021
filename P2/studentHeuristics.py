ac9e19f94b82e3865cb000ba5d1b29ec7d991ccb
Fgp910 <f.gutierrezpena910@gmail.com>
2021-03-13 00:48:26 +0100

    Implementa la heurística pero muy mal y no son horas de arreglarlo

--- P2/studentHeuristics.py
+++ P2/studentHeuristics.py
@@ -1,9 +1,26 @@
-from game import (
-    TwoPlayerGameState,
-)
-from tournament import (
-    StudentHeuristic,
-)
+from game import TwoPlayerGameState
+from heuristic import complex_evaluation_function
+from reversi import from_array_to_dictionary_board
+from tournament import StudentHeuristic
+from typing import Callable
+
+
+def general_evaluation_function(state: TwoPlayerGameState, func: Callable[[TwoPlayerGameState], float]) -> float:
+    if state.end_of_game:
+        scores = state.scores
+        score_difference = scores[0] - scores[1]
+        if state.is_player_max(state.player1):
+            return score_difference
+        if state.is_player_max(state.player2):
+            return - score_difference
+        else:
+            raise ValueError('Player MAX not defined')
+
+    return func(state)
+
+
+def is_x_position(pos) -> bool:
+    return pos in [(2, 2), (2, 7), (7, 2), (7, 7)]
 
 
 class MySolution1(StudentHeuristic):
@@ -11,19 +28,29 @@ class MySolution1(StudentHeuristic):
         return "2301_04_sol1"
 
     def evaluation_function(self, state: TwoPlayerGameState) -> float:
-        if state.end_of_game:
-            scores = state.scores
-            # Evaluation of the state from the point of view of MAX
-            score_difference = scores[0] - scores[1]
-        else:
-            successors = state.game.generate_successors(state)
-            # Minimize the number of your opponent moves (for MAX).
-            score_difference = - len(successors)
+        return complex_evaluation_function(state)
 
+
+class MySolution3(StudentHeuristic):
+    def get_name(self) -> str:
+        return "2301_04_sol3"
+
+    def evaluation_function(self, state: TwoPlayerGameState) -> float:
+        return general_evaluation_function(state, self.evaluate)
+
+    def evaluate(self, state: TwoPlayerGameState) -> float:
+        successors = state.game.generate_successors(state)
+        total = len(successors)
+        exes = 0.
+        for suc in successors:
+            for pos in suc.board.keys():
+                if is_x_position(pos) and pos not in state.board:
+                    exes += 1
+
+        score = exes / total
         if state.is_player_max(state.player1):
-            return score_difference
-        elif state.is_player_max(state.player2):
-            return - score_difference
+            return - score
+        if state.is_player_max(state.player2):
+            return score
         else:
             raise ValueError('Player MAX not defined')
-
